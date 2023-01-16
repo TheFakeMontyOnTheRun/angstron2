9942214f01e9cb5d828f66631cea6661cef6f7ec
Daniel Monteiro <themontyhacker@gmail.com>
2023-01-16 21:28:35 +0000

    fix: build script to not require OpenGL libs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 clear
 echo =========================================================
 
-g++  -w -g  -DNETWORKSUPPORT -DSUPPORT_BITMAP_FONTS -DSUPPORT_SDL_GFX -DWGA -DFPU -DBZK_NO_USE_OPENGL    -fno-exceptions -DDEBUG -ffast-math -oangstron2  BZKmain.cpp AppStart.cpp SDL_gfx/SDL_gfxPrimitives.c sprig/*.c `sdl-config --cflags --libs` -lSDL_mixer -lGL -lGLU 
+g++  -w -g  -DNETWORKSUPPORT -DSUPPORT_BITMAP_FONTS -DSUPPORT_SDL_GFX -DWGA -DFPU -DBZK_NO_USE_OPENGL    -fno-exceptions -DDEBUG -ffast-math -oangstron2  BZKmain.cpp AppStart.cpp SDL_gfx/SDL_gfxPrimitives.c sprig/*.c `sdl-config --cflags --libs` -lSDL_mixer 
 #strip angstron2
 
 
